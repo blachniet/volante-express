eb625cb6236a82d998483f27886bad2db47d97c5
Mike Smiley <msmiley@alumni.rice.edu>
2021-05-17 15:34:38 +0000

    remove crud overlay-wasnt used enough; code cleanup and comments

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const cors = require('cors');
 //
 module.exports = {
   name: 'VolanteExpress',
+  props: {
+    bind: '127.0.0.1',        // ip address to bind, use 0.0.0.0 for all interfaces
+    port: 3000,               // port to bind
+    https: false,             // flag for https mode
+    key: null,                // key buffer, e.g. from fs.readFileSync('key.pem')
+    cert: null,               // cert buffer, e.g. from fs.readFileSync('cert.pem')
+    logging: true,            // flag for adding volante logging middleware to express
+    cors: '*',                // cors origin, see https://www.npmjs.com/package/cors#configuration-options
+    middleware: [],           // middleware array, for manually entered (see test code at bottom)
+    errorOnBindFail: true,    // flag for whether this module should request a shutdown if it can't bind
+    bodyParserLimit: '100mb', // body parser size limit
+  },
   init() {
     // instantiate the express app
     this.app = express();
@@ -36,27 +48,12 @@ module.exports = {
       this.$debug('adding middleware through event');
       this.app && this.app.use(...middleware);
     },
-    'VolanteExpress.crud'(obj) {
-      this.registerCrud(obj);
-    },
     'VolanteExpress.start'() {
       this.start();
     },
     'VolanteExpress.stop'() {
       this.stop();
     },
-  },
-  props: {
-    bind: '127.0.0.1',
-    port: 3000,
-    https: false,
-    key: null,
-    cert: null,
-    logging: true,
-    cors: '*',
-    middleware: [],
-    errorOnBindFail: true,
-    bodyParserLimit: '100mb',
   },
   methods: {
     start() {
@@ -70,9 +67,7 @@ module.exports = {
 
       // add error handler middleware just before starting
       this.$debug('adding default error handler');
-      this.app.use((err, req, res, next) => {
-        this.errorMiddleware(err, req, res);
-      });
+      this.app.use(this.errorMiddleware);
 
       if (this.https && this.key && this.cert) {
         let options = {
@@ -84,26 +79,12 @@ module.exports = {
         this.server = require('http').createServer(this.app);
       }
 
-      this.server.on('error', (err) => {
-        if (this.errorOnBindFail) {
-          switch (err.code) {
-            case 'EADDRINUSE':
-              this.$error(`Port ${this.port} is already in use, is another instance running?`);
-              break;
-            default:
-              this.$error('unable to open listen port', err);
-          }
-          this.$shutdown(); // system-wide dealbreaker
-        } else {
-          this.$warn(`Couldn't bind ${this.port}`);
-        }
-      });
+      // handle express error
+      this.server.on('error', this.handleExpressError);
+      // handle express close
+      this.server.on('close', this.handleExpressClose);
 
-      this.server.on('close', () => {
-        this.$log(`closed ${this.https?'HTTPS':'HTTP'} server`);
-      });
-
-      // start
+      // start express
       this.server.listen(this.port, this.bind, () => {
         this.$emit('VolanteExpress.listening', {
           bind: this.bind,
@@ -113,8 +94,26 @@ module.exports = {
         this.$log(`listening in ${this.app.get('env')} mode for ${this.https?'HTTPS':'HTTP'} on ${this.bind}:${this.port}`);
       });
     },
+    handleExpressError(err) {
+      if (this.errorOnBindFail) {
+        switch (err.code) {
+          case 'EADDRINUSE':
+            this.$error(`Port ${this.port} is already in use, is another instance running?`);
+            break;
+          default:
+            this.$error('unable to open listen port', err);
+        }
+        this.$shutdown(); // system-wide dealbreaker
+      } else {
+        this.$warn(`Couldn't bind ${this.port}, set errorOnBindFail=true to exit here`);
+      }
+    },
+    handleExpressClose() {
+      this.$log(`closed ${this.https?'HTTPS':'HTTP'} server`);
+    },
     //
     // middleware to check cors if it's set. passthrough otherwise
+    //
     checkCors(req, res, next) {
       cors({ origin: this.cors, credentials: true })(req, res, next);
     },
@@ -145,7 +144,7 @@ module.exports = {
     //
     // add error handling middleware
     //
-    errorMiddleware(err, req, res) {
+    errorMiddleware(err, req, res, next) {
       if (this.logging) {
         this.$error('express error', {
           method: req.method, // HTTP method
@@ -161,61 +160,6 @@ module.exports = {
         res.status(500).send();
       }
     },
-    //
-    // add routes for a generic CRUD bridge between express and volante
-    //
-    registerCrud(obj) {
-      this.$debug('setting up simple CRUD bridge between express.js and Volante');
-      this.$debug(`CRUD operations on ${obj.path} will be mapped to ${obj.name}`);
-      if (obj.name && obj.path) {
-        // create
-        this.app.post(obj.path, (req, res) => {
-          if (req.body) {
-            this.$emit('volante.create', obj.name, req.body, (err, result) => {
-              if (err) return res.status(500).send(err);
-              res.send(result);
-            });
-          }
-        });
-        // read all
-        this.app.get(obj.path, (req, res) => {
-          this.$emit('volante.read', obj.name, {}, (err, docs) => {
-            if (err) return res.status(500).send(err);
-            res.send(docs);
-          });
-        });
-        // query (pass body through as query)
-        this.app.post(`${obj.path}/query`, (req, res) => {
-          this.$emit('volante.read', obj.name, req.body, (err, docs) => {
-            if (err) return res.status(500).send(err);
-            res.send(docs);
-          });
-        });
-        // read by id
-        this.app.get(`${obj.path}/:id`, (req, res) => {
-          this.$emit('volante.read', obj.name, req.params.id, (err, docs) => {
-            if (err) return res.status(500).send(err);
-            res.send(docs);
-          });
-        });
-        // update
-        this.app.put(`${obj.path}/:id`, (req, res) => {
-          this.$emit('volante.update', obj.name, req.params.id, req.body, (err, result) => {
-            if (err) return res.status(500).send(err);
-            res.send(result);
-          });
-        });
-        // delete
-        this.app.delete(`${obj.path}/:id`, (req, res) => {
-          this.$emit('volante.delete', obj.name, req.params.id, (err, result) => {
-            if (err) return res.status(500).send(err);
-            res.send(result);
-          });
-        });
-      } else {
-        this.warn('registerCrud called without required parameters');
-      }
-    },
   },
 };
 
@@ -228,7 +172,6 @@ if (require.main === module) {
 
   hub.emit('VolanteExpress.update', {
     port: 3000,
-    errorOnBindFail: false,
     middleware: [
       (req, res) => {
         res.send('<div style="height:100%;display:flex;flex-direction:column;align-items:center;justify-content:center;font-size:32px;font-family:sans-serif;font-weight:300;"><span>hello from volante-express</span><span>❤︎</span></div>');
@@ -237,5 +180,4 @@ if (require.main === module) {
   });
 
   hub.emit('VolanteExpress.start');
-
 }
